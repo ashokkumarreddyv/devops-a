b48bd05079fc94c0e472da461e9d67d44ade600b
ashokv <ashok3akr@gmail.com>
2026-03-12 14:59:08 +0530

    bucket

--- environments/main.tf
+++ environments/main.tf
@@ -1,18 +1,18 @@
-module "vpc" {
-  source = "../modules/vpc"
+# module "vpc" {
+#   source = "../modules/vpc"
 
-  vpc_cidr    = var.vpc_cidr
-  subnet_cidr = var.subnet_cidr
-}
+#   vpc_cidr    = var.vpc_cidr
+#   subnet_cidr = var.subnet_cidr
+# }
 
-module "ec2" {
-  source = "../modules/ec2"
+# module "ec2" {
+#   source = "../modules/ec2"
 
-  instance_type          = var.instance_type
-  ami_id                 = var.ami_id
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids
-}
+#   instance_type          = var.instance_type
+#   ami_id                 = var.ami_id
+#   subnet_id              = var.subnet_id
+#   vpc_security_group_ids = var.vpc_security_group_ids
+# }
 
 module "s3" {
   source = "../modules/S3"
--- environments/variables.tf
+++ environments/variables.tf
@@ -1,11 +1,11 @@
-variable "instance_type" {}
-variable "ami_id" {}
-variable "name" {}
-variable "vpc_cidr" {}
-variable "subnet_cidr" {}
-variable "bucket_name" {}
-variable "subnet_id" {}
-variable "vpc_security_group_ids" {}
+# variable "instance_type" {}
+# variable "ami_id" {}
+# variable "name" {}
+# variable "vpc_cidr" {}
+# variable "subnet_cidr" {}
+  variable "bucket_name" {}
+# variable "subnet_id" {}
+# variable "vpc_security_group_ids" {}
 
 
   
