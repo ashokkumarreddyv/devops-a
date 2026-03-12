382787845265f84f437bc771e44982705ff5c5e3
ashokv <ashok3akr@gmail.com>
2026-03-12 15:15:36 +0530

    instance

--- environments/providers.tf
+++ environments/providers.tf
@@ -1,10 +1,10 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "aws-devops-bucket-ash2933"
-#     key    = "dev/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "aws-devops-bucket-ash2933"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 
 
 
