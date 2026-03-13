9ba09bb79a9ac3452d9cae3f7ec19d8919044e66
ashokv <ashok3akr@gmail.com>
2026-03-13 15:07:37 +0530

    r

--- modules/S3/main.tf
+++ modules/S3/main.tf
@@ -29,7 +29,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-lock-table"
+  name         = "ashok"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
